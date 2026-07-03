7a4f18166ceadc33c5de5e982a62471adaf8af4b
Patcharepon Wechjarern <patcharapon.w@ku.th>
2026-07-03 11:45:53 +0700

    complete all ex. in file

--- labs/week01-lab/example/first_program.py
+++ labs/week01-lab/example/first_program.py
@@ -10,8 +10,11 @@ print("My name is Your Name")
 # Write three print statements that display:
 # Line 1: Your name
 # Line 2: Your age
-# Line 3: Your favorite hobbyๅ
+# Line 3: Your favorite hobby
 # Write your code here:
+print("My name is Patcharapon")
+print("I am 19 Years old")
+print("My favorite Hobby is Sleeping")
 
 
 # Exercise 4: Print with Different Content
@@ -21,8 +24,9 @@ print("My name is Your Name")
 # - A sentence with spaces
 
 # Write your code here:
-
-
+print(19)
+print("Hello")
+print("Hi My Name is Arm")
 
 # Exercise 5: Understanding Print Behavior
 # What happens when you run these? Try to predict before running:
@@ -33,11 +37,11 @@ print("Third", "line", "with", "multiple", "arguments")
 # Exercise 6: Comments Practice
 # Add comments above each print statement explaining what it does
 
-# 
+# It will print Welcome to Python programming!
 print("Welcome to Python programming!")
 
-# 
+# It will print Python is fun!
 print("Python is fun!")
 
-# 
+# It will print I am learning to code!
 print("I am learning to code!")
