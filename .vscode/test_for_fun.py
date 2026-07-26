d4702ec1b6836b90a50cff84458a2a8e88e32370
Patcharapon Wechjaroen <patcharapon.w@ku.th>
2026-07-26 23:14:49 +0700

    fix something in tax

--- .vscode/test_for_fun.py
+++ .vscode/test_for_fun.py
@@ -1,4 +1,3 @@
-balance = int(input("How much of your salary: "))
 total_tax = 0
 balance_tax = 0
 tax1 = 0
@@ -10,37 +9,44 @@ tax6 = 0
 tax7 = 0
 tax8 = 0
 
-if balance > 0 :
-    tax1 = (min(balance , 150000)- 0) * 0
-    total_tax += tax1
+def calculate_tax(balance,total_tax):
+    if balance > 0 :
+        tax1 = (min(balance , 150000)- 0) * 0
+        total_tax += tax1
 
-if balance > 150000  :
-    tax2 = (min(balance , 300000)- 150000) * 0.05 
-    total_tax += tax2
+    if balance > 150000  :
+        tax2 = (min(balance , 300000)- 150000) * 0.05 
+        total_tax += tax2
 
-if balance > 300000 :
-    tax3 = (min(balance , 500000)- 300000) * 0.10
-    total_tax += tax3
+    if balance > 300000 :
+        tax3 = (min(balance , 500000)- 300000) * 0.10
+        total_tax += tax3
 
-if balance > 500000  :
-    tax4 = (min(balance , 750000)- 500000) * 0.15
-    total_tax += tax4
+    if balance > 500000  :
+        tax4 = (min(balance , 750000)- 500000) * 0.15
+        total_tax += tax4
   
-if balance > 750000 :
-    tax5 = (min(balance , 1000000)- 750000)* 0.20
-    total_tax += tax5
+    if balance > 750000 :
+        tax5 = (min(balance , 1000000)- 750000)* 0.20
+        total_tax += tax5
 
-if balance > 1000000  :
-    tax6 = (min(balance , 2000000)- 1000000) * 0.25 
-    total_tax += tax6
+    if balance > 1000000  :
+        tax6 = (min(balance , 2000000)- 1000000) * 0.25 
+        total_tax += tax6
 
-if balance > 2000000 :
-    tax7 = (min(balance , 5000000)- 2000000) * 0.30
-    total_tax += tax7
+    if balance > 2000000 :
+        tax7 = (min(balance , 5000000)- 2000000) * 0.30
+        total_tax += tax7
 
-if balance > 5000000 :
-    tax8 = (balance - 5000000) * 0.35
-    total_tax += tax8
+    if balance > 5000000 :
+        tax8 = (balance - 5000000) * 0.35
+        total_tax += tax8
+
+    return total_tax
+
+balance = int(input("How much of your salary: "))
+
+total_tax = calculate_tax(balance,total_tax)
 
 print("You Income: ",balance)
 print("ภาษีขั้นที่ 1: ",tax1)
