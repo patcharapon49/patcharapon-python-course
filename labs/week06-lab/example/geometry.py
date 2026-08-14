c06ce6b17ab0b8a8d0a3425c6f5194188ea16190
Patcharepon Wechjarern <patcharapon.w@ku.th>
2026-08-14 12:08:51 +0700

    function grometry and test that how much I understand .

--- labs/week06-lab/example/geometry.py
+++ labs/week06-lab/example/geometry.py
@@ -19,3 +19,36 @@ def calculate_trangle_area(height, base):
 print("Calculating triangle areas:")
 calculate_trangle_area(5, 3)
 calculate_trangle_area(10, 7)
+
+"""
+เขียน function แปลงหน่วยเงิน ที่สามารถแปลงเงินจาก
+THB <--> USD .. 1 USD = 32 THB
+THB <--> JPY .. 100 JPY = 22 THB
+
+โดยใช้ชื่อและการใช้งาน
+function_convert_currency(100,"USD")
+
+แสดงผลการทำงานหน้าจอ
+100 THB = 3.3 USD
+
+และทดสอยผลการทำงาน function ที่ตัวเองเรียกด้วย
+"""
+
+currency = 0.0
+type = ""
+def function_convert_currency(currency,type):
+    if type == "USD":
+        new_currency = currency / 32
+        print(f"{currency} THB = {new_currency:.1f} USD")
+
+    if type == "JPY":
+        new_currency = currency / 22
+        print(f"{currency} THB = {new_currency:.1f} JPY")
+
+    if type != "USD" or "JPY":
+        print("We have only USD/JPY")
+        quit()
+        
+currency = float(input("How many you want to convert: "))
+type = str(input("What type you want to covert USD/JPY: "))
+function_convert_currency(currency,type)
