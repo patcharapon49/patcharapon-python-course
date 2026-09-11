25de81141538ae65825a6adbcae53fe37b82d5e7
Patcharapon Wechjaroen <patcharapon.w@ku.th>
2026-09-11 12:06:26 +0700

    test somthing at home

--- .vscode/test.py
+++ .vscode/test.py
@@ -1,15 +1,7 @@
-students = [ [201, "Arun Kumar", "11", 87],
-             [202, "Meenakshi R", "12", 82],
-             [203, "Rajeshwari Nair", "12", 100],
-             [204, "Suresh Babu", "11", 76],
-             [205, "Anitha Subramaniam", "11", 99] ]
-
-
-
-centum = []
-for student in students:
-        if student[3] == 100:
-            centum.append(student[0:2])
-
-print(centum)
-
+TRAVERSE(head):
+    current ← head
+    WHILE current ≠ None DO
+        VISIT(current.data)
+        current ← current.next
+    END WHILE
+END
