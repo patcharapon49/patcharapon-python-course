e6f3e0e17f7081f4c2ed39746e16c78e73ae649b
Patcharapon Wechjaroen <patcharapon.w@ku.th>
2026-09-25 12:04:32 +0700

    Study about Class

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -12,13 +12,35 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
-        pass
+        self.area = self.width * self.length
+        return self.area
 
     # Method to get the perimeter
     def get_perimeter(self):
-        pass
+        self.perimeter = self.length * 4
+        return self.perimeter
 
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+"""
+เขียนclasscircle ที่ทำงานคล้ายคลึงกับ classrectangle
+
+"""
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def get_area(self):
+        self.area = self.radius * (3.14 ** 2)
+        return f"Your Area of Circle is {self.area:.2f}"
+
+    def get_perimeter(self):
+        self.perimeter = 2 * 3.14 * self.radius
+        return f"Your Perimeter of Circle is {self.perimeter:.2f}"
+
+myCircle = Circle(10)
+print(myCircle.get_area())
+print(myCircle.get_perimeter())
